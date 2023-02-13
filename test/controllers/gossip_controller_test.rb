7b9b0af9417d2d286f45565cce7915181f45e507
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-02-13 19:34:15 +0100

    Database & first pages

---
+++ test/controllers/gossip_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class GossipControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get gossip_home_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get gossip_contact_url
+    assert_response :success
+  end
+
+  test "should get team" do
+    get gossip_team_url
+    assert_response :success
+  end
+end
